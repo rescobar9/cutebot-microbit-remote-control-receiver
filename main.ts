e48edc01c6c29b71c54cccbebf9bebb4f6acf56e
Rudy Escobar <67067784+rescobar9@users.noreply.github.com>
2023-01-15 19:38:26 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (0 == 0) {
+        cuteBot.turnleft()
+    } else if (0 == 0) {
+        cuteBot.turnleft()
+    } else if (0 == 0) {
+        cuteBot.forward()
+    }
+})
+radio.setGroup(1)
